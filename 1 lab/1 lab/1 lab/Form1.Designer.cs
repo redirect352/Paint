f8246290f441a73f622f4f3a32e6a1cca3296782
Ponomarev Nikita <79397499+redirect352@users.noreply.github.com>
2021-02-27 21:08:07 +0300

    1 лабораторная
    Рисование прямых, многоугольников, эллипса и ломанной.

---
+++ 1 lab/1 lab/1 lab/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace _1_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ClearButt = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(810, 497);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // ClearButt
+            // 
+            this.ClearButt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClearButt.Location = new System.Drawing.Point(853, 12);
+            this.ClearButt.Name = "ClearButt";
+            this.ClearButt.Size = new System.Drawing.Size(93, 33);
+            this.ClearButt.TabIndex = 1;
+            this.ClearButt.Text = "Очистить";
+            this.ClearButt.UseVisualStyleBackColor = true;
+            this.ClearButt.Click += new System.EventHandler(this.ClearButt_Click);
+            this.ClearButt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ClearButt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Линия",
+            "Прямоугольник",
+            "Эллипс",
+            "Многоугольник",
+            "Ломанная"});
+            this.comboBox1.Location = new System.Drawing.Point(853, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(93, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(886, 91);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(828, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Вершины";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 546);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ClearButt);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ClearButt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 1 lab/1 lab/1 lab/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_lab
+{
+    public partial class Form1 : Form
+    {
+        enum drMode { drModeLine, drModeRect, drModeEllipse, drModePolygon, drModeBrLine };
+        Graphics gr;
+        int x = -1, y = -1, x1 = -1, y1 = -1;
+        bool isMoving = false, shiftPressed = false, controlPressed = false;
+        Pen p;
+        drMode dr = drMode.drModeLine;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            gr = panel1.CreateGraphics();
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            p = new Pen(Color.Black);
+            p.StartCap = p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            p.Width = 6;
+            comboBox1.SelectedItem = comboBox1.Items[0];
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+        
+            x = e.X;
+            y = e.Y;
+            isMoving = true;
+            if (dr == drMode.drModeLine)
+                gr.DrawLine(p, e.Location, new Point(x+1, y+1));
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!isMoving)
+                return;
+
+            if (shiftPressed && dr == drMode.drModeLine )
+                gr.DrawLine(p, new Point(x, y), e.Location);
+            if (dr == drMode.drModeRect || dr == drMode.drModeEllipse)
+            {
+
+                int buf;
+                var tmp = new Point(e.X, e.Y);
+                if (tmp.X < x)
+                {
+                    buf = tmp.X;
+                    tmp.X = x;
+                    x = buf;
+                }
+                if (tmp.Y < y)
+                {
+                    buf = tmp.Y;
+                    tmp.Y = y;
+                    y = buf;
+                }
+                if (dr == drMode.drModeRect)
+                    gr.DrawRectangle(p, x, y, tmp.X - x, tmp.Y - y);
+                else
+                    gr.DrawEllipse(p, x, y, tmp.X - x, tmp.Y - y);
+            }
+            if (dr == drMode.drModePolygon)
+            {
+                int r = (int)(e.Y - y)/2, x1,y1;
+              
+                var center = new PointF(x+r, y+r);
+                int n = (int)numericUpDown1.Value;
+                double angle = Math.PI * 2 / n;
+                Point[] points = new Point[n];
+                for (int i =0; i< n; i++)
+                {
+                    x1 = (int)(center.X + Math.Cos(i * angle) * r);
+                    y1 = (int)(center.Y + Math.Sin(i * angle) * r);
+                    points[i].X = x1;
+                    points[i].Y = y1;
+
+                }
+
+                
+                gr.DrawPolygon(p, points) ;
+
+
+
+            }
+            isMoving = false;
+            
+            x = -1; y = -1; 
+            
+
+
+
+        }
+
+        private void ClearButt_Click(object sender, EventArgs e)
+        {
+            gr.Clear(panel1.BackColor);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            numericUpDown1.Visible = false;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    dr = drMode.drModeLine;
+                    break;
+                case 1:
+                    dr = drMode.drModeRect;
+                    break;
+                case 2:
+                    dr = drMode.drModeEllipse;
+                    break;
+                case 3:
+                    dr = drMode.drModePolygon;
+                    label1.Visible = true;
+                    numericUpDown1.Visible = true;
+                    break;
+                case 4:
+                    dr = drMode.drModeBrLine;
+                    break;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (dr != drMode.drModeBrLine)
+                return;
+
+            if (x1 == -1 || y1 == -1)
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                return;
+            }
+            else
+            {
+                gr.DrawLine(p, new Point(x1, y1), e.Location);
+                x1 = e.X;
+                y1 = e.Y;
+            }
+
+                
+            
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMoving || dr != drMode.drModeLine)
+            { return; }
+
+
+            if (e.X != x && e.Y!=y && !shiftPressed && !controlPressed)
+            {
+                gr.DrawLine(p,new Point(x,y), e.Location);
+                x = e.X; y = e.Y;
+            }
+
+            if ( controlPressed)
+            {
+                int deltaX = e.X - x, deltaY = e.Y - y;
+                double tga = Math.Abs((double)deltaY / (deltaX + 0.01));
+
+                if (tga < Math.Tan(Math.PI/4))
+                {
+                    gr.DrawLine(p, new Point(x, y), new Point(e.X, y));
+                    //x = e.X;
+                }
+
+
+                if (tga >= Math.Tan(Math.PI / 4))
+                {
+                    gr.DrawLine(p, new Point(x, y), new Point(x, e.Y));
+                    //y = e.Y;
+                }
+
+
+            }
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            shiftPressed = false;
+            controlPressed = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            shiftPressed = e.Shift;
+            controlPressed = e.Control;
+        }
+    }
+}
