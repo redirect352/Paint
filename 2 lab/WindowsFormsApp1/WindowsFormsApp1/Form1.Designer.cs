687270d0b2aa9c03574f9177366aa6fa14b3f939
Ponomarev Nikita <79397499+redirect352@users.noreply.github.com>
2021-03-07 17:44:37 +0300

    second lab
    созданы классы для фигур. Возможность выбора толщины линии, цвета линии

---
+++ 2 lab/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.RigthPolygon = new System.Windows.Forms.CheckBox();
+            this.TopsLabel = new System.Windows.Forms.Label();
+            this.PreDrawTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PenWidthBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.PenColorButton = new System.Windows.Forms.Button();
+            this.PenColorPanel = new System.Windows.Forms.Panel();
+            this.FillColorButton = new System.Windows.Forms.Button();
+            this.FillColorPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PenWidthBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(844, 474);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(106, 23);
+            this.ClearButton.TabIndex = 1;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Линия",
+            "Прямоугольник",
+            "Эллипс",
+            "Многоугольник"});
+            this.comboBox1.Location = new System.Drawing.Point(844, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(917, 64);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // RigthPolygon
+            // 
+            this.RigthPolygon.AutoSize = true;
+            this.RigthPolygon.Location = new System.Drawing.Point(879, 39);
+            this.RigthPolygon.Name = "RigthPolygon";
+            this.RigthPolygon.Size = new System.Drawing.Size(90, 17);
+            this.RigthPolygon.TabIndex = 4;
+            this.RigthPolygon.Text = "Правильный";
+            this.RigthPolygon.UseVisualStyleBackColor = true;
+            // 
+            // TopsLabel
+            // 
+            this.TopsLabel.Location = new System.Drawing.Point(841, 64);
+            this.TopsLabel.Name = "TopsLabel";
+            this.TopsLabel.Size = new System.Drawing.Size(70, 29);
+            this.TopsLabel.TabIndex = 5;
+            this.TopsLabel.Text = "Количество вершин";
+            // 
+            // PreDrawTimer
+            // 
+            this.PreDrawTimer.Interval = 50;
+            this.PreDrawTimer.Tick += new System.EventHandler(this.PreDrawTimer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(811, 494);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // PenWidthBar
+            // 
+            this.PenWidthBar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PenWidthBar.Location = new System.Drawing.Point(844, 96);
+            this.PenWidthBar.Maximum = 20;
+            this.PenWidthBar.Name = "PenWidthBar";
+            this.PenWidthBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.PenWidthBar.Size = new System.Drawing.Size(45, 170);
+            this.PenWidthBar.TabIndex = 6;
+            this.PenWidthBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.PenWidthBar.Value = 6;
+            this.PenWidthBar.Scroll += new System.EventHandler(this.PenWidthBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(841, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Толщина";
+            // 
+            // PenColorButton
+            // 
+            this.PenColorButton.Location = new System.Drawing.Point(829, 298);
+            this.PenColorButton.Name = "PenColorButton";
+            this.PenColorButton.Size = new System.Drawing.Size(60, 40);
+            this.PenColorButton.TabIndex = 8;
+            this.PenColorButton.Text = "Цвет линии";
+            this.PenColorButton.UseVisualStyleBackColor = true;
+            this.PenColorButton.Click += new System.EventHandler(this.PenColorButton_Click);
+            // 
+            // PenColorPanel
+            // 
+            this.PenColorPanel.BackColor = System.Drawing.Color.Black;
+            this.PenColorPanel.Location = new System.Drawing.Point(910, 297);
+            this.PenColorPanel.Name = "PenColorPanel";
+            this.PenColorPanel.Size = new System.Drawing.Size(40, 41);
+            this.PenColorPanel.TabIndex = 9;
+            this.PenColorPanel.Click += new System.EventHandler(this.PenColorButton_Click);
+            // 
+            // FillColorButton
+            // 
+            this.FillColorButton.Location = new System.Drawing.Point(829, 344);
+            this.FillColorButton.Name = "FillColorButton";
+            this.FillColorButton.Size = new System.Drawing.Size(60, 40);
+            this.FillColorButton.TabIndex = 10;
+            this.FillColorButton.Text = "Цвет заливки";
+            this.FillColorButton.UseVisualStyleBackColor = true;
+            this.FillColorButton.Click += new System.EventHandler(this.FillColorButton_Click);
+            // 
+            // FillColorPanel
+            // 
+            this.FillColorPanel.BackColor = System.Drawing.Color.White;
+            this.FillColorPanel.Location = new System.Drawing.Point(910, 343);
+            this.FillColorPanel.Name = "FillColorPanel";
+            this.FillColorPanel.Size = new System.Drawing.Size(40, 41);
+            this.FillColorPanel.TabIndex = 10;
+            this.FillColorPanel.Click += new System.EventHandler(this.FillColorButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 522);
+            this.Controls.Add(this.FillColorPanel);
+            this.Controls.Add(this.FillColorButton);
+            this.Controls.Add(this.PenColorPanel);
+            this.Controls.Add(this.PenColorButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PenWidthBar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TopsLabel);
+            this.Controls.Add(this.RigthPolygon);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ClearButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PenWidthBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox RigthPolygon;
+        private System.Windows.Forms.Label TopsLabel;
+        private System.Windows.Forms.Timer PreDrawTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar PenWidthBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button PenColorButton;
+        private System.Windows.Forms.Panel PenColorPanel;
+        private System.Windows.Forms.Button FillColorButton;
+        private System.Windows.Forms.Panel FillColorPanel;
+    }
+}
+
---
+++ 2 lab/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics gr;
+        Pen p;
+        Figure CurrentFig;
+        Bitmap p1= new Bitmap(1000,1000), p2;
+
+        List<Figure> figures = new List<Figure>();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            gr = Graphics.FromImage(p1);
+
+
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            p = new Pen(Color.Black);
+            p.StartCap = p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            p.Width = PenWidthBar.Value;
+            CurrentFig = new StraigthLine(-1, -1, gr, p, FillColorPanel.BackColor);
+
+            pictureBox1.Image = p1;
+
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            CurrentFig.StartPoint = new Point(e.X, e.Y);
+            PreDrawTimer.Enabled = true;
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (CurrentFig.StartPoint.X < 0)
+                return;
+
+            if (!PreDrawTimer.Enabled)
+            {
+
+
+                p2 = new Bitmap(p1);
+                pictureBox1.Image = p2;
+                gr = Graphics.FromImage(p2);
+                CurrentFig.DrawPanel = gr;
+
+
+                CurrentFig.EndPoint = e.Location;
+                gr.Dispose();
+                PreDrawTimer.Enabled = true;
+               
+               
+                gr.Dispose();
+            }
+
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            PreDrawTimer.Enabled = false;
+            try
+            {
+
+                gr = Graphics.FromImage(p1);
+                CurrentFig.DrawPanel = gr;
+                CurrentFig.EndPoint = new Point(e.X, e.Y);
+                CurrentFig.StartPoint = new Point(-1,-1);
+                pictureBox1.Image = p1;
+            }
+            catch { }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            gr = Graphics.FromImage(p1);
+
+            gr.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = p1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RigthPolygon.Visible = false;
+            numericUpDown1.Visible = false;
+            TopsLabel.Visible = false;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                
+
+                case 0:
+                    CurrentFig = new StraigthLine(-1, -1, gr, p, FillColorPanel.BackColor);
+                    break;
+                case 1 :
+                    CurrentFig = new Rectangle(-1, -1, gr, p, FillColorPanel.BackColor);
+                    break;
+                case 2:
+                    CurrentFig = new Ellipse(-1, -1, gr, p, FillColorPanel.BackColor);
+                    break;
+                case 3:
+                    RigthPolygon.Checked = true;
+                    RigthPolygon.Visible = true;
+                    numericUpDown1.Visible = true;
+                    TopsLabel.Visible = true;
+                    CurrentFig = new RigthPolygon(-1, -1, gr, p, FillColorPanel.BackColor);
+                    break;
+            }
+        }
+
+        private void PenWidthBar_Scroll(object sender, EventArgs e)
+        {
+            p.Width = PenWidthBar.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (CurrentFig is RigthPolygon)
+                (CurrentFig as RigthPolygon).TopAmount = (int)numericUpDown1.Value;
+        }
+
+        private void PenColorButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                PenColorPanel.BackColor = colorDialog1.Color;
+                p.Color = colorDialog1.Color;
+            }
+        }
+
+        private void FillColorButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FillColorPanel.BackColor = colorDialog1.Color;
+                
+            }
+        }
+
+        private void PreDrawTimer_Tick(object sender, EventArgs e)
+        {
+            PreDrawTimer.Enabled = false;
+        }
+    }
+
+
+
+
+
+
+
+    public abstract class Figure
+    {
+        public Graphics DrawPanel;
+        protected Point startPoint;
+        protected Point endPoint = new Point(-1,-1);
+        public Pen DrPen;
+        public Color FillColor;
+
+
+        public Figure(int x0, int y0, Graphics gr, Pen p, Color Fc)
+        {
+            startPoint = new Point(x0, y0);
+            DrawPanel = gr;
+            DrPen = p;
+            FillColor = Fc;
+        }
+
+        public Point StartPoint
+        {
+            get
+            {
+                return startPoint;
+            }
+            set
+            {
+                startPoint = value;
+
+            }
+        }
+
+        public virtual Point EndPoint
+        {
+            get
+            {
+                return endPoint;
+            }
+            set
+            {
+                endPoint = value;
+
+            }
+
+        }
+
+        protected void FindLeftTopPoint( ref Point p1, ref Point p2)
+        {
+            int buf;
+            if (p2.X < p1.X)
+            {
+                buf = p2.X;
+                p2.X = p1.X;
+                p1.X = buf;
+            }
+            if (p2.Y < p1.Y)
+            {
+                buf = p2.Y;
+                p2.Y = p1.Y;
+                p1.Y = buf;
+            }
+        }
+
+
+    }
+
+    public class StraigthLine : Figure
+    {
+        public StraigthLine(int x0, int y0, Graphics gr, Pen p, Color Fc) : base(x0, y0, gr, p, Fc) { }
+
+        public override Point EndPoint
+        {
+            get
+            {
+                return endPoint;
+            }
+            set
+            {
+                endPoint = value;
+                DrawPanel.DrawLine(DrPen, startPoint, endPoint);
+
+            }
+        }
+
+
+
+    }
+
+    public class Rectangle : Figure
+    {
+
+        public Rectangle(int x0, int y0, Graphics gr, Pen p, Color Fc) : base(x0, y0, gr, p, Fc) { }
+
+
+        public override Point EndPoint
+        {
+            get
+            {
+                return endPoint;
+            }
+            set
+            {
+                endPoint = value;
+                Point p1 = new Point(startPoint.X, startPoint.Y);
+
+                FindLeftTopPoint(ref startPoint, ref endPoint);
+                DrawPanel.DrawRectangle(DrPen, startPoint.X, startPoint.Y, endPoint.X - startPoint.X, endPoint.Y - startPoint.Y);
+                startPoint = p1;
+            }
+        }
+
+    }
+
+    public class Ellipse : Figure
+    {
+
+        public Ellipse(int x0, int y0, Graphics gr, Pen p, Color Fc) : base(x0, y0, gr, p, Fc) { }
+
+        public override Point EndPoint
+        {
+            get
+            {
+                return endPoint;
+            }
+            set
+            {
+                endPoint = value;
+                Point p1 = new Point(startPoint.X, startPoint.Y);
+                FindLeftTopPoint(ref startPoint, ref endPoint);
+                DrawPanel.DrawEllipse(DrPen, startPoint.X, startPoint.Y, endPoint.X - startPoint.X, endPoint.Y - startPoint.Y);
+
+                startPoint = p1;
+            }
+        }
+
+    }
+
+    public class RigthPolygon : Figure
+    {
+
+        public RigthPolygon(int x0, int y0, Graphics gr, Pen p, Color Fc) : base(x0, y0, gr, p, Fc) { }
+
+        public int TopAmount = 3;
+
+        public override Point EndPoint
+        {
+            get
+            {
+                return endPoint;
+            }
+            set
+            {
+                endPoint = value;
+
+                int r = (int)(endPoint.X - startPoint.X) / 2, x1, y1;
+
+                var center = new PointF(StartPoint.X + r, startPoint.Y + r);
+                
+                double angle = Math.PI * 2 / TopAmount;
+                Point[] points = new Point[TopAmount];
+                for (int i = 0; i < TopAmount; i++)
+                {
+                    x1 = (int)(center.X + Math.Cos(i * angle + Math.PI/3) * r);
+                    y1 = (int)(center.Y + Math.Sin(i * angle+ Math.PI / 3) * r);
+                    points[i].X = x1;
+                    points[i].Y = y1;
+
+                }
+                DrawPanel.DrawPolygon(DrPen,points);
+
+
+            }
+        }
+    }
+
+}
